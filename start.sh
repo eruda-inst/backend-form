560f9796f59d3b0f0f84fc9e5b692f05d84927ea
iamreinaldo <iamreinaldoneto@icloud.com>
2025-08-28 11:42:56 -0300

    alembic

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 set -e
 cd /app
 if [ "${RUN_MIGRATIONS:-0}" = "1" ]; then
-  alembic -c app/alembic.ini upgrade head
+  alembic -c alembic.ini upgrade head
 fi
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
